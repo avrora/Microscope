cba66514ade3401e4cb8142478334156128cd312
Alex Benito <alex@avrora.io>
2014-02-07 19:47:35 +0100

    Added posts list / templates & controllers

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,24 @@
+/**
+ * Created by alex on 7/02/2014.
+ */
+
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        author: 'Sacha Greif',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        author: 'Tom Coleman',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        author: 'Tom Coleman',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
